7ea183e540d4fba4d3c2497f29a6614bea8a0b1e
Arshia <arshiags@gmail.com>
2025-01-19 19:40:56 -0500

    Implemented functions for remainder

--- js/assignment.js
+++ js/assignment.js
@@ -6,3 +6,20 @@ const checkIfEvenElement = document.getElementById("check-if-even");
 const lostNumbersElement = document.getElementById("lost-numbers");
 
 const lostNumbers = [4, 8, 15, 16, 23, 42];
+
+function returnTheRemainder(num1, num2) {
+    return num1 % num2;
+}
+
+function checkIfEven(num) {
+    return num % 2 === 0;
+}
+
+function getTheFourthElement(arr) {
+    return arr[3];
+}
+
+// Render answers to the page
+remainderElement.textContent = returnTheRemainder(10, 3);  // Example: Display remainder of 10 divided by 3
+checkIfEvenElement.textContent = checkIfEven(4);  // Example: Check if 4 is even
+lostNumbersElement.textContent = getTheFourthElement(lostNumbers);  // Get the 4th element from the array
--- js/index.js
+++ js/index.js
@@ -2,3 +2,28 @@
 // that are not declared
 "use strict"
 
+// function addTwoNumbers(a, b) {
+//     return a + b;
+// }
+// console.log(addTwoNumbers(3, 5));
+
+// function multiplyNumbers(x, y) {
+//     return x * y;
+// }
+// console.log(multiplyNumbers(4, 6));
+
+// function greet(name) {
+//     console.log('Hello, ' + name + '!');
+// }
+// greet('Arshia');
+
+// function greetWithDefault(name = 'Guest') {
+//     console.log('Hi, ' + name + '!');
+// }
+// greetWithDefault();
+// greetWithDefault('Arshia');
+
+
+
+
+
